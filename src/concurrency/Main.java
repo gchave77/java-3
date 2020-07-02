585a869c15b8bdaf6fefbb7df8115af8df140899
gabe chavez <gabe.chavez77@gmail.com>
2020-07-01 22:50:09 -1000

    completed Reasoning thread

--- src/concurrency/Main.java
+++ src/concurrency/Main.java
@@ -4,5 +4,8 @@ public class Main {
    
     public static void main(String[] args) {
          //run all of your threads from this main class.
+        Reasoning reasoning = new Reasoning();
+        reasoning.start();
+        System.out.println("Test");
     }
 }
